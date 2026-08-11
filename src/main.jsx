7a44aee8c90e7a03965c99d0032338be88cffbb5
adamweads-art <adam.w.eads@gmail.com>
2026-08-10 22:56:42 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Note: /react, not the Next.js path Vercel's setup page shows by default.
+import { Analytics } from "@vercel/analytics/react";
 import MatchweekBoard from "./MatchweekBoard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MatchweekBoard />
+    <Analytics />
   </React.StrictMode>
 );
